f003c739daa303bbc5910f67cc7cb6c64c947d2a
hamletzhang <HamletZhang@qq.com>
2026-01-27 17:59:10 +0800

    Update __init__.py

--- labelme/__init__.py
+++ labelme/__init__.py
@@ -14,7 +14,7 @@ __appname__ = "LabelmeAI"
 # e.g., 1.0.0a0, 1.0.0a1, 1.0.0b0, 1.0.0rc0, 1.0.0, 1.0.0.post0
 
 
-__version__ = "2026.01.26.0"
+__version__ = "2026.01.27.0"
 
 
 QT4 = QT_VERSION[0] == "4"
