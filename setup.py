d5514099151b6bd31fa34795e4d28b99e533587d
杨培文 (Yang Peiwen) <915505626@qq.com>
2026-09-26 16:44:37 +0800

    build: 更新图片内标注核心库依赖

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def main():
         version=version,
         description="DLCV-Labelme AI",
         install_requires=[
-            "dlcv-core>=2026.9.22.0a0",
+            "dlcv-core>=2026.9.26.2a0",
             "pyqt-toast-notification",
             "open3d",
         ],
